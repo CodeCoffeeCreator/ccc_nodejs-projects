215600d0c338748bdba61dd0f9c09beae2064ce5
Denny Pearce <digital.y.coffee@gmail.com>
2022-04-28 02:45:50 +0300

    work with os, fs and path modules

--- weather-cli-app/weather.js
+++ weather-cli-app/weather.js
@@ -1,11 +1,23 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import { printHelp } from './services/log.service.js';
+import { printHelp, printError, printSuccess } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue('token', token);
+    printSuccess('Токен сохранен');
+  } catch (e) {
+    printError(e.message);
+  }
+};
 
 const initCLI = () => {
   const args = getArgs(process.argv);
 
   if (args.h) printHelp();
+
+  if (args.t) return saveToken(args.t);
 };
 
 initCLI();
