0a801c610a3a7c6579ea07a570084e802850982e
Denny Pearce <digital.y.coffee@gmail.com>
2022-04-28 14:31:30 +0300

    do some work to make simple server on nodejs, learn theory about it and try use it

---
+++ node-express-api/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import { userRouter } from './users/users.js';
+
+const port = 8000;
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('Time ', Date.now());
+  next();
+});
+
+app.get('/hello', (req, res) => {
+  throw new Error('ERROR!!!');
+});
+
+app.use('/users', userRouter);
+
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(401).send(err.message);
+});
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
