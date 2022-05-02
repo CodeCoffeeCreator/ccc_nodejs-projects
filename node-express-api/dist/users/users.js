a776f446088cc645f4bd49657e89c7a08f75bad9
Denny Pearce <digital.y.coffee@gmail.com>
2022-05-02 16:49:52 +0300

    write main init class for app

--- node-express-api/dist/users/users.js
+++ node-express-api/dist/users/users.js
@@ -1,5 +1,12 @@
-import express from 'express';
-const userRouter = express.Router();
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userRouter = void 0;
+const express_1 = __importDefault(require("express"));
+const userRouter = express_1.default.Router();
+exports.userRouter = userRouter;
 userRouter.use((req, res, next) => {
     console.log('Users handler');
     next();
@@ -10,4 +17,3 @@ userRouter.post('/login', (req, res) => {
 userRouter.post('/register', (req, res) => {
     res.send('register');
 });
-export { userRouter };
