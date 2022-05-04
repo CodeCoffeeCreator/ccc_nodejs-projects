1f98b3510fef775114eb65275b084d16f3ff246b
Denny Pearce <digital.y.coffee@gmail.com>
2022-05-04 21:02:30 +0300

    improve our DI

--- node-express-api/dist/main.js
+++ node-express-api/dist/main.js
@@ -1,29 +1,24 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.appContainer = exports.app = exports.appBindings = void 0;
 const inversify_1 = require("inversify");
 const app_1 = require("./app");
 const exeption_filter_1 = require("./errors/exeption.filter");
 const logger_service_1 = require("./logger/logger.service");
 const types_1 = require("./types");
 const users_controller_1 = require("./users/users.controller");
+exports.appBindings = new inversify_1.ContainerModule((bind) => {
+    bind(types_1.TYPES.ILogger).to(logger_service_1.LoggerService);
+    bind(types_1.TYPES.ExeptionFilter).to(exeption_filter_1.ExeptionFilter);
+    bind(types_1.TYPES.UserController).to(users_controller_1.UserController);
+    bind(types_1.TYPES.Application).to(app_1.App);
+});
 function bootstrap() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const appContainer = new inversify_1.Container();
-        appContainer.bind(types_1.TYPES.ILogger).to(logger_service_1.LoggerService);
-        appContainer.bind(types_1.TYPES.ExeptionFilter).to(exeption_filter_1.ExeptionFilter);
-        appContainer.bind(types_1.TYPES.UserController).to(users_controller_1.UserController);
-        appContainer.bind(types_1.TYPES.Application).to(app_1.App);
-        const app = appContainer.get(types_1.TYPES.Application);
-        yield app.init();
-    });
+    const appContainer = new inversify_1.Container();
+    appContainer.load(exports.appBindings);
+    const app = appContainer.get(types_1.TYPES.Application);
+    app.init();
+    return { appContainer, app };
 }
-bootstrap();
+_a = bootstrap(), exports.app = _a.app, exports.appContainer = _a.appContainer;
