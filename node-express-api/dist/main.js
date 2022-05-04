c8fdb4f81aae3b7fed7eaa4cec6a983485be8aff
Denny Pearce <digital.y.coffee@gmail.com>
2022-05-04 20:52:54 +0300

    implements inversify to our API

--- node-express-api/dist/main.js
+++ node-express-api/dist/main.js
@@ -9,14 +9,20 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const inversify_1 = require("inversify");
 const app_1 = require("./app");
 const exeption_filter_1 = require("./errors/exeption.filter");
 const logger_service_1 = require("./logger/logger.service");
+const types_1 = require("./types");
 const users_controller_1 = require("./users/users.controller");
 function bootstrap() {
     return __awaiter(this, void 0, void 0, function* () {
-        const logger = new logger_service_1.LoggerService();
-        const app = new app_1.App(logger, new users_controller_1.UserController(logger), new exeption_filter_1.ExeptionFilter(logger));
+        const appContainer = new inversify_1.Container();
+        appContainer.bind(types_1.TYPES.ILogger).to(logger_service_1.LoggerService);
+        appContainer.bind(types_1.TYPES.ExeptionFilter).to(exeption_filter_1.ExeptionFilter);
+        appContainer.bind(types_1.TYPES.UserController).to(users_controller_1.UserController);
+        appContainer.bind(types_1.TYPES.Application).to(app_1.App);
+        const app = appContainer.get(types_1.TYPES.Application);
         yield app.init();
     });
 }
