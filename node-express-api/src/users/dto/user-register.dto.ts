fb5ee66bf14690234078c5931a9cea59994aeb4d
Denny Pearce <digital.y.coffee@gmail.com>
2022-05-05 23:20:31 +0300

    add data validation

--- node-express-api/src/users/dto/user-register.dto.ts
+++ node-express-api/src/users/dto/user-register.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class UserRegisternDto {
+	@IsEmail({}, { message: 'Wrong email' })
 	email: string;
+
+	@IsString({ message: 'Input password' })
 	password: string;
+
+	@IsString({ message: 'Input name' })
 	name: string;
 }
