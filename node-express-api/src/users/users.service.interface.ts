feae955e93e5946af5ef22b713df784803c1ed6a
Denny Pearce <digital.y.coffee@gmail.com>
2022-05-06 21:08:47 +0300

    create users repository

--- node-express-api/src/users/users.service.interface.ts
+++ node-express-api/src/users/users.service.interface.ts
@@ -1,8 +1,8 @@
+import { UserModel } from '@prisma/client';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisternDto } from './dto/user-register.dto';
-import { User } from './user.entity';
 
 export interface IUserService {
-	createUser: (dto: UserRegisternDto) => Promise<User | null>;
+	createUser: (dto: UserRegisternDto) => Promise<UserModel | null>;
 	validateUser: (dto: UserLoginDto) => Promise<boolean>;
 }
--- node-express-api/src/users/users.service.ts
+++ node-express-api/src/users/users.service.ts
@@ -1,21 +1,29 @@
+import { UserModel } from '@prisma/client';
 import { inject, injectable } from 'inversify';
 import { IConfigService } from '../config/config.service.interface';
 import { TYPES } from '../types';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisternDto } from './dto/user-register.dto';
 import { User } from './user.entity';
+import { IUsersRepository } from './users.repository.interface';
 import { IUserService } from './users.service.interface';
 
 @injectable()
 export class UserService implements IUserService {
-	constructor(@inject(TYPES.ConfigService) private configService: IConfigService) {}
+	constructor(
+		@inject(TYPES.ConfigService) private configService: IConfigService,
+		@inject(TYPES.UserRepository) private usersRepository: IUsersRepository,
+	) {}
 
-	async createUser({ email, name, password }: UserRegisternDto): Promise<User | null> {
+	async createUser({ email, name, password }: UserRegisternDto): Promise<UserModel | null> {
 		const newUser = new User(email, name);
 		const salt = this.configService.get('SALT');
-		console.log(salt);
 		await newUser.setPassword(password, Number(salt));
-		return null;
+		const existedUser = await this.usersRepository.find(email);
+		if (existedUser) {
+			return null;
+		}
+		return this.usersRepository.create(newUser);
 	}
 
 	async validateUser(dto: UserLoginDto): Promise<boolean> {
